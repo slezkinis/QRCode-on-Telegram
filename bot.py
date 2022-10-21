dbd121a8b7ab34553cab83bc1300f202068fa0d6
slezkinis <slezkin.is@mail.ru>
2022-10-21 22:40:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,17 @@ load_dotenv()
 telegram_token = os.environ['TG_TOKEN']
 bot = telebot.TeleBot(token=telegram_token)
 HELP = '''
-/help - помощь
+/start - помощь
 /qrcode <ссылка> - получить qrcode
+/git - получить ссылку на проект
 '''
+STRAT = 'Привет! Это бот для создания QR кодов. Нужго ввести команду /qrcode и далее, через пробел указать ссылку. Ниже представлены доступны команды:'
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(message.chat.id, STRAT)
     bot.send_message(message.chat.id, HELP)
-
 
 @bot.message_handler(commands=['qrcode'])
 def qrcode(message):
@@ -31,6 +33,11 @@ def qrcode(message):
         bot.send_photo(message.chat.id, file)
         os.remove('qrcode.png')
     bot.send_message(message.chat.id, 'Вот Ваш QRCode!! Спасибо за использование!')
+
+
+@bot.message_handler(commands=['git'])
+def git(message):
+    bot.send_message(message.chat.id, 'https://github.com/slezkinis/QRcode-on-Telegram')
 
 
 bot.polling(none_stop=True)
